342df6e3c3c60ddf0def69a5eb1b860ba7c375f4
SohamMhatre09 <150317894+SohamMhatre09@users.noreply.github.com>
2024-02-17 17:42:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,19 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const userRouter = require("./routes/user");
-
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/user", userRouter)
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
     res.send("Welcome To API for GFG Hackathon");
 });
 try {
     mongoose.connect('mongodb+srv://eventmanager:eventmanager@eventmanager44.ihhelur.mongodb.net/', {dbName: "EventManager44" });
     console.log('MongoDB connected');
-    app.listen(3000, () => console.log('Server running on port 3000'));
+    app.listen(PORT, () => console.log('Server running on port 3000'));
 }
 catch (error) {
     console.log('MongoDB connection failed');
